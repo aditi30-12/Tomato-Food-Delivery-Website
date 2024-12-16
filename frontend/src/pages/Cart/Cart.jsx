55468c38677d9064ea91db630ba4f91756f7e85e
aditi30-12 <aditipatel4m@gmail.com>
2024-12-16 12:52:26 +0530

    signIn

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import {StoreContext} from '../../context/storecontext'
 
 const Cart = () => {
+
+  const {cartItems,food_list,removeFromCart}= useContext(StoreContext);
   return (
-    <div>Cart</div>
+    <div></div>
   )
 }
 
